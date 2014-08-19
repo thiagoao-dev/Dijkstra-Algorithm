f0d2dbc31a1572f28346d546c8541dc2866126af
Thiago Augustus de Oliveira <thiagoaugustusdeoliveira@gmail.com>
2014-08-19 00:37:49 -0300

    Inclusão do arquivo json

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,1 +1,16 @@
 __author__ = 'ThiagoAugustus'
+
+import json
+
+# Lê o arquivo json
+def readJson():
+    # Abre o arquivo conforme o caminho informado
+    with open(input("grafod.json")) as json_file:
+        json_file = json.load(json_file)
+        # Testa o objeto
+        assert isinstance(json_file, object)
+        return json_file
+
+grafo = readJson()
+
+print(grafo)
